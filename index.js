0043b5824de1160ace655d8d204529e78a80d6f1
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-21 15:30:10 +0100

    dokonceny dizajn, zaciatok JS funkcie

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+function generatePassword(characters) {
+    return characters[Math.floor(Math.random()*characters.length)];
+}
+
+console.log(characters)
+
+
+
+ 
