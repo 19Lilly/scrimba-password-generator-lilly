ee8c731e74daa5ab67c96a7c9ab0280e35899d33
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-25 09:26:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 let password = ""
 let password1El = document.getElementById("password-1")
-console.log(password1El)
 
+function sum (a,b){
+    return a + b;
+}
 
-
-
+console.log(sum(3,7));
 
 function generatePassword(characters) {
     for (let i = 0; i <16; i++){
@@ -18,6 +19,8 @@ password1El.text = password;
 }
 
 generatePassword(characters)
+
+console.log(password);
 
 
 
