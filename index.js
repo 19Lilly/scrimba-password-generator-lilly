13d28070764d2f02d60223db106acb03d02fde42
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-25 13:15:50 +0100

    funkcia v js funguje

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
 let password1El = document.getElementById("password-1")
 let password2El = document.getElementById("password-2")
 
-function generatePassword() {
-    let password = ""
-    for (let i = 0; i < 15; i++){
-    password1El.textContent += (characters[Math.floor(Math.random()*characters.length)]);  
-    password2El.textContent += (characters[Math.floor(Math.random()*characters.length)]);  
+function generatePassword (){
+    password1El.textContent = passwordGenerator();
+    password2El.textContent = passwordGenerator();
 }
 
-}
+function passwordGenerator() {
+    let password = ""
+    let passwordLength = document.getElementById("password-length").value
+    let withoutSymbols = document.getElementById("without-symbols").checked
+    let withoutNumbers = document.getElementById("without-numbers").checked
+
+    if (withoutSymbols === true && withoutNumbers === true) {
+        for (let i = 0; i < passwordLength; i++){
+            password += (letters[Math.floor(Math.random()*letters.length)]); 
+              
+        } return password;
+
+        } else if (withoutSymbols === true && withoutNumbers === false) {
+            for (let i = 0; i < passwordLength; i++){
+                password += (letters.concat(numbers)[Math.floor(Math.random()*letters.concat(numbers).length)]); 
+               
+            } return password;
+        
+        } else if(withoutSymbols === false && withoutNumbers === true) {
+            for (let i = 0; i < passwordLength; i++){
+                password += (letters.concat(symbols)[Math.floor(Math.random()*letters.concat(symbols).length)]); 
+                   
+            } return password;
+            
+        } else
+        for (let i = 0; i < passwordLength; i++){
+            password += (letters.concat(numbers, symbols)[Math.floor(Math.random()*letters.concat(numbers, symbols).length)]); 
+           
+        } return password;
+        
+    }
+
+
 
 
 
