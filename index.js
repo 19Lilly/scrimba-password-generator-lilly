df0a2a0dc789b1b522c0c63610ba3231fc717d12
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-25 10:50:15 +0100

    funkcia generovania hesla funkcna

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let password = ""
 let password1El = document.getElementById("password-1")
+let password2El = document.getElementById("password-2")
 
-function sum (a,b){
-    return a + b;
+function generatePassword() {
+    let password = ""
+    for (let i = 0; i < 15; i++){
+    password1El.textContent += (characters[Math.floor(Math.random()*characters.length)]);  
+    password2El.textContent += (characters[Math.floor(Math.random()*characters.length)]);  
 }
 
-console.log(sum(3,7));
-
-function generatePassword(characters) {
-    for (let i = 0; i <16; i++){
-    return password[i] += characters[Math.floor(Math.random()*characters.length)];
-    
-}
-password1El.text = password;
 }
 
-generatePassword(characters)
 
-console.log(password);
+
+
+
+
+
+
+
 
 
 
